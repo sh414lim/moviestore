58a7bb4b289a195d0431d7ea1090ab460acef0a4
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-06-01 16:26:51 +0900

    Detail

--- src/Routes/Detail/DetailPresent.js
+++ src/Routes/Detail/DetailPresent.js
@@ -10,11 +10,23 @@ const Container =styled.div`
   height:110vh ;
   display: block;
   margin-left: 200px;
+  position: relative;
+  height: 150vh;
+  font-weight: 600;
+`;
+
+const Title=styled.span`
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 60px;
+    padding-top: 30px;
 `;
 
 const ImageContainer=styled.div`
 position: absolute;   //*** */중요
-width: 20%;
+width: 30%;
 height: 70%;
 margin-top: 100px;
 margin-left: 100px;
@@ -28,35 +40,96 @@ border-radius: 20px;
 }
 `;
 
-const ListDiv=styled.div``;
-
-const List=styled.span`
+const ListDiv=styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
+width: 50%;
+margin-top: 150px;
+margin-left: 600px;
+border-bottom: 1px solid white;
+font-size: 20px;
+line-height: 30px;
 `;
 
-const Title=styled.span`
-
+const CASH=styled.div`
     display: flex;
-    justify-content: center;
     align-items: center;
-    font-size: 60px;
-    padding-top: 30px;
+    margin-left: 600px;;
+margin-top: 100px;
+    font-size: 30px;
 `;
+
+const Popular=styled.div`
+display: flex;
+align-items: center;
+margin-left: 600px;
+margin-top: 5px;
+`;
+
+const RunTime=styled.div`
+margin-left: 600px;
+margin-top: 10px;
+font-weight: 600;
+`;
+
+const Background=styled.div`
+height: calc(100%- 200px);
+position: absolute;
+background-image: url(${props=>props.bgImage});
+background-size: cover;
+background-position: center center;
+width: 100%;
+height: 100%;
+filter: blur(2px);
+z-index: -1;
+top: 0;
+left:0;
+opacity: 0.8;
+`;
+
+
+
+
+
+
+
+
+
+
+
 
 const DetailPresenter=({result,loading,error})=>loading ? null :(
+    <>
 <Container>
+    <Background bgImage={result.poster_path ? `https://image.tmdb.org/t/p/original${result.poster_path}` : require("../../assests/noPosterSmall.PNG")}/>
     <Title>{result.original_title ? result.original_title : result.original_name }</Title>
-<ImageContainer bgImage={result.poster_path ? `https://image.tmdb.org/t/p/original${result.poster_path}` : require("../../assests/noPosterSmall.PNG")}/> 
-<ListDiv>
-<List>
-</List>
+        <ImageContainer bgImage={result.poster_path ? `https://image.tmdb.org/t/p/original${result.poster_path}` : require("../../assests/noPosterSmall.PNG")}/>
+    <CASH>
+        $:{result.id}
+    </CASH>
+    <RunTime>
+    {result.runtime ? result.runtime : result.episode_run_time[0]} min
+    </RunTime>
+    <Popular>
+        Popular:{result.popularity}
+    </Popular>
+    <Popular>
+    {result.genres &&
+                result.genres.map((genre, index) =>
+                  index === result.genres.length - 1
+                    ? genre.name
+                    : `${genre.name} / `
+                )}
+    </Popular>
+    <Popular>
+    </Popular>
 
-</ListDiv>
-
+    <ListDiv>
+        {result.overview}
+    </ListDiv>
 </Container>
-
+</>
 )
 
 export default DetailPresenter;
