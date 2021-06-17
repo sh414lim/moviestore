849270c15f2a2ce0d4344b365837405ca1f1a1f3
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-06-17 09:27:21 +0900

    .

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,11 +1,14 @@
 //리덕스 모듈
 //Ducks 패턴 
 //액션 생성함수 액션함수 리듀서 를 모두 하나의 파일에서 관리    
-
-
 import {createAction,handleActions} from "redux-actions";
+import product from "immer";
 
-const SAMPLE_ACTION='auth/SAMPLE_ACTION';
+
+const CHANGE_FIELD
+
+
+
 
 export const sampleAction=createAction(SAMPLE_ACTION);
 
