ae6b9c671f536435cea588f41218eced88c68b6b
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-06-22 19:32:47 +0900

    수정ㅇ

--- src/Components/EvenPractice.js
+++ src/Components/EvenPractice.js
@@ -1,30 +1,9 @@
-import React, { useReducer } from "react";
+import React from "ract";
 
-const Reducer =(state,action)=>{
-    switch(action.type){
-        case "PLUS":
-        return({value:state.value + 1})
-        case "MINUS":
-        return({value:state.value -1})
-        default:
-            return state;
-    }
+const Test=()=>{
     
-}
-
-const Counter =()=>{
-
-    const[state,dispatch]=useReducer(Reducer,{value:0})
-
+    
     return(
-            <>
-            <p>{state.value}</p>
         
-            <button onClick={()=>dispatch({type:"PLUS"})}>+1</button>
-            <button onClick={()=>dispatch({type:"MINUS"})}>-1</button>
-            
-            </>
-        )
+    )
 }
-
-export default Counter;
