b9c8f812b0d21bdf2c211ca8858ceed6e502a2a6
sh414lim <sh414lim@github.com>
2021-07-13 13:36:08 +0900

    게시판

--- src/About.js
+++ src/About.js
@@ -1,32 +1,38 @@
-/*global kakao*/
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import KakaoMap from './kakaoMap';
-
-class MapContent extends Component {
-  componentDidMount() {
-    const script = document.createElement('script');
-    script.async = true;
-    script.src =
-      'https://dapi.kakao.com/v2/maps/sdk.js?appkey=b401e67c4abf766538ced0dbca2fbb53&autoload=false';
-    document.head.appendChild(script);
-
-    script.onload = () => {
-      kakao.maps.load(() => {
-        let container = document.getElementById('Mymap');
-        let options = {
-          center: new kakao.maps.LatLng(37.506502, 127.053617),
-          level: 7,
-        };
-
-        const map = new window.kakao.maps.Map(container, options);
-      });
-    };
-  }
-
-  render() {
-    return <KakaoMap></KakaoMap>;
-  }
-}
-
-export default MapContent;
+ /*global kakao*/
+ import React, { Component } from "react";
+ import styled from "styled-components";
+ 
+ class MapContent extends Component {
+     componentDidMount() {
+       const script = document.createElement("script");
+       script.async = true;
+       script.src =
+         "https://dapi.kakao.com/v2/maps/sdk.js?appkey=b401e67c4abf766538ced0dbca2fbb53&autoload=false";
+       document.head.appendChild(script);
+ 
+       script.onload = () => {
+         kakao.maps.load(() => {
+           let container = document.getElementById("Mymap");
+           let options = {
+             center: new kakao.maps.LatLng(37.506502, 127.053617),
+             level: 7
+           };
+   
+           const map = new window.kakao.maps.Map(container, options);
+        
+         });
+       };
+     }
+   
+     render() {
+       return <MapContents id="Mymap">1</MapContents>;
+     }
+   }
+   
+   const MapContents = styled.div`
+     width: 100%;
+     height: 1000px;
+     height: 500px;
+   `;
+   
+   export default MapContent;
---
+++ src/writer.js
@@ -0,0 +1,11 @@
+import React,{useRef,useEffect} from "react";
+import Quill from "quill";
+import 'quill/dist/quill.bubble.css';
+import styled from "styled-components";
+import Responsive from "./Components/conmon/Responsive";
+
+
+const EditorBlock=styled(Responsive)`
+ padding-top : 5rm;
+ padding-bottom:5rem;
+`;
